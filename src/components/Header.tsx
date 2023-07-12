cba7fba80427852993a8dcfd489b62ab4b5975a4
Samuel Dutra Rodrigues de Abreu <samueld.r.abreu@gmail.com>
2023-07-12 09:28:30 -0300

    style: Adding Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css'
+import todoLogo from '../assets/rocket.svg'
+
+export default function Header() {
+
+  return (
+    <header className={styles.header}>
+      <img src={todoLogo} alt="Logo" />
+      <span>to</span><span>do</span>
+    </header>
+      
+    
+
+  )
+}
+
