3261a9915209306bb93c6c6a23a62dcee394c31f
Samuel Dutra Rodrigues de Abreu <samueld.r.abreu@gmail.com>
2023-07-14 09:56:44 -0300

    perf (header): form of new tasks moved to the ListTasks component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,14 @@
 import styles from './Header.module.css'
 import todoLogo from '../assets/rocket.svg'
-import { PlusCircle} from 'phosphor-react'
 
 export default function Header() {
 
   return (
     <div>
       <header className={styles.header}>
-
         <img src={todoLogo} alt="Logo" />
         <span>to</span><span>do</span>
       </header>
-
-      <footer className={styles.footer}>
-        <form className={styles.task}> 
-          <textarea 
-            name="newTask"
-            placeholder='Adicionar uma nova tareda'
-            required
-          />
-          <button 
-            type='submit'
-          >
-            Criar
-            <PlusCircle className={styles.buttonPlusIcon} size={20} />
-          </button>
-        </form>
-      </footer>
   </div>
   )
 }
--- src/components/ListTasks.tsx
+++ src/components/ListTasks.tsx
@@ -1,21 +1,54 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import styles from './ListTasks.module.css'
-import { ClipboardText } from 'phosphor-react'
+import { ClipboardText, PlusCircle } from 'phosphor-react'
+import Task from './Task';
+
+export interface Task {
+  id: number;
+  task: string;
+  finished: false;
+}
 
 export default function ListTasks() {
-  
+  const [tasks, setTasks] = useState([])
+  const [finishedTasks, setFinishedTasks] = useState([])
+
+  const [newTaskText, setNewTaskText] = useState('');
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault()
+    
+    setTasks([...tasks, newTaskText])
+    setNewTaskText('')
+  }
+
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        <div className={styles.info}>
-          <span className={styles.Newtasks}>Tarefas Criadas 0</span>
-          <span className={styles.Endtasks}>Concluídas 0</span>
-        </div>
-      </header>
+      <form 
+        className={styles.task}
+        onSubmit={handleCreateNewTask}
+      > 
+        <textarea 
+          name="newTask"
+          placeholder='Adicionar uma nova tareda'
+          required
+        />          
+        <button 
+          type='submit'
+        >
+          Criar
+          <PlusCircle className={styles.buttonPlusIcon} size={20} />
+        </button>
+      </form>
+      <div className={styles.info}>
+        <span className={styles.Newtasks}>Tarefas Criadas {tasks.length}</span>
+        <span className={styles.Endtasks}>Concluídas {tasks.length}</span>
+      </div>
       <footer className={styles.footer}>
         <ClipboardText size={56} className={styles.icon}/>
         <span>Você ainda não tem tarefas cadastradas</span>
         <p>Crie tarefas e organize seus itens a fazer</p>
+        <Task content='Beber agua' /> 
       </footer>
     </div>
   );
