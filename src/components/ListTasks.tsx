28084d8d8bb68569966f2c5a8838e842d3ad9e67
Samuel Dutra Rodrigues de Abreu <samueld.r.abreu@gmail.com>
2023-07-18 23:16:52 -0300

    feat (task): added difference between completed task and pending task

--- src/components/ListTasks.tsx
+++ src/components/ListTasks.tsx
@@ -2,23 +2,42 @@ import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 import styles from './ListTasks.module.css'
 import { ClipboardText, PlusCircle } from 'phosphor-react'
 import Task from './Task';
+import uuid from 'uuid-random';
 
-export interface Task {
-  id: number;
-  task: string;
-  finished: false;
+export interface TypeTask {
+  id: string;
+  content: string;
+  isFinished: boolean;
 }
 
 export default function ListTasks() {
-  const [tasks, setTasks] = useState([])
-  const [finishedTasks, setFinishedTasks] = useState([])
-
+  const [tasks, setTasks] = useState<TypeTask[]>([])
+  const [completedTasksCount, setCompletedTasksCount] = useState(0)
+ 
   const [newTaskText, setNewTaskText] = useState('');
+
+  function listTasks() {
+    if (tasks.length <= 0){
+      return(
+        <>
+        <ClipboardText size={56} className={styles.icon}/>
+        <span>Você ainda não tem tarefas cadastradas</span>
+        <p>Crie tarefas e organize seus itens a fazer</p>
+        </>
+      );
+    }
+  }
 
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault()
-    
-    setTasks([...tasks, newTaskText])
+
+    const newTask: TypeTask = {
+      "id": uuid(),
+      "content": newTaskText,
+      "isFinished": false
+    };
+
+    setTasks([...tasks, newTask])
     setNewTaskText('')
   }
 
@@ -32,12 +51,29 @@ export default function ListTasks() {
   }
 
   function deleteTask(taskToDelete: string) {
-    const taskWithoutDeleteOne = tasks.filter(task => {
-      return task !== taskToDelete;
+    
+    const taskWithoutDeleteOne = tasks.filter(task => {  
+      if (task.id === taskToDelete && task.isFinished){
+        setCompletedTasksCount(completedTasksCount - 1)
+      }  
+      return task.id !== taskToDelete;
     })
     setTasks(taskWithoutDeleteOne)
   }
 
+  function setStatusTask(taskToSet: string) {
+    let count = 0;
+    tasks.map(task => {
+      if (task.id === taskToSet) {
+        task.isFinished = !task.isFinished;
+      }
+      if (task.isFinished){
+        count =count + 1;
+      }
+    })
+    setCompletedTasksCount(count)
+  }
+  
   return (
     <div className={styles.container}>
       <form 
@@ -60,19 +96,22 @@ export default function ListTasks() {
         </button>
       </form>
       <div className={styles.info}>
-        <span className={styles.Newtasks}>Tarefas Criadas {tasks.length}</span>
-        <span className={styles.Endtasks}>Concluídas {tasks.length}</span>
+        <span className={styles.Newtasks}>Tarefas Criadas <span>{tasks.length}</span></span>
+        <span className={styles.Endtasks}>Concluídas <span>{completedTasksCount} de {tasks.length}</span></span>
       </div>
+      
       <footer className={styles.footer}>
-        <ClipboardText size={56} className={styles.icon}/>
-        <span>Você ainda não tem tarefas cadastradas</span>
-        <p>Crie tarefas e organize seus itens a fazer</p>
-        {tasks.map(task => {
+      {listTasks()}
+      {tasks.map(task => {
           return <Task 
-          key={task}
-          content={task}
-          onDeleteTask={deleteTask}/> 
-        })}
+          key={task.id}
+          id={task.id}
+          content={task.content}
+          isFinished={task.isFinished}
+          onDeleteTask={deleteTask}
+          onFinishedTask={setStatusTask}
+        />
+      })}
       </footer>
     </div>
   );
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,24 +1,65 @@
 import { Trash } from "phosphor-react";
 import styles from './Task.module.css';
+import { useState } from "react";
 
-interface TaskProps{
-  content: string;
+import circle from '../assets/circle.svg'
+import check from '../assets/check.svg'
+import { TypeTask } from "./ListTasks";
+
+interface TaskProps extends TypeTask{
   onDeleteTask: (comment: string) => void;
+  onFinishedTask: (comment: string) => void;
 }
 
-export default function Task({content, onDeleteTask}: TaskProps) {
+export default function Task({id, content, isFinished, onDeleteTask, onFinishedTask}: TaskProps) {
+  const [finishedTask, setFinishedTask] = useState<boolean>(isFinished);
+
+  function task() {
+    if (finishedTask){
+      return (
+        <div className={styles.task}>
+          <button 
+            className={styles.completedCheckBox}
+            onClick={handleFinishedTask}
+          >
+            <img src={check} alt="" />
+          </button>
+          <p className={styles.finishedText}>{content}</p> 
+          <button onClick={handleDeleteTask} title='Deletar tarefa'>
+            <Trash size={18} />
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.task}>
+          <button 
+            className={styles.checkBox}
+            onClick={handleFinishedTask}
+          >
+            <img src={circle} alt="" />
+          </button>
+          <p className={styles.taskText}>{content}</p> 
+          <button onClick={handleDeleteTask} title='Deletar tarefa'>
+            <Trash size={18} />
+          </button>
+        </div>
+      );
+    }
+    
+  }
   function handleDeleteTask() {
-    onDeleteTask(content);
+   
+    onDeleteTask(id);
+    
   }
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.teste}></div>
-      <p>{content}</p> 
-      <button onClick={handleDeleteTask} title='Deletar tarefa'>
-        <Trash size={18} />
-      </button>
-      
-    </div>
-  );
+
+  function handleFinishedTask(){
+    
+    onFinishedTask(id)
+    setFinishedTask(!finishedTask)
+  }
+
+  return task();
 }
